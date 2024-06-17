036a6e38a21f9eadb50b2f382c4cf90843ef128e
Zac-Milioli <zacmilioli@gmail.com>
2024-06-17 13:03:42 -0300

    primeiros códigos

--- ADM_firstpage.py
+++ ADM_firstpage.py
@@ -1,17 +1,60 @@
 import streamlit as st
+import smtplib
+import email.message
+from random import randint
 
-st.set_page_config(page_title="ADM", page_icon=":bar_chart:", layout="wide")
+# password = st.secrets['EMAIL_PASSWORD']
+authorization_list = ["519249","215333","885531","618511","572447","869808","795177","371314","349847","725307","152101","687085","930430","862644","631116","202600","226685","675900","850068","502866","499124","119076","194394","850663","501970","343110","604899","873815","873732","332027","229599","195539","642693","113458","973402","798801","651854","588405","940423","774287","620429","797880","652906","713421","149751","419936","947997","372791","250261","469589","499024","854435","413249","856289","381478","617604","476102","738816","105552","961946","633041","305899","566873","299503","924671","502313","124981","563472","999154","314874","205129","685368","875393","430502","433164","209055","254352","479366","755502","800045","218490","280180","666648","651533","831232","711813","665228","484822","798532","145640","886177","791129","482785","486436","611493","464165","665697","786134","344894","828499"]
+password = st.secrets['EMAIL_PASSWORD']
 
-st.title("ADM")
+def mail_me(mail_person:str, perguntas_e_respostas:dict):
+    corpo_email = f'{perguntas_e_respostas}'
+    msg = email.message.Message()
+    msg['Subject'] = f'RESPOSTAS-{mail_person}'
+    msg['From'] = 'escritorios.qai.bot@gmail.com'
+    msg['To'] = 'escritorios.qai.bot@gmail.com'
+    msg.set_payload(corpo_email)
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
 
-with st.form("my_form"):
-    st.markdown("**Formulário testes**")
-    st.radio("Qual seu tipo de escritório?", ['Salas separadas', 'Coworking', 'Home office'])
-    st.slider("Quantas pessoas trabalham com você?", 1, 10)
-    cep = st.text_input("Digite seu CEP")
+def mail_auth_code(mail_person:str):
+    auth_code = authorization_list[randint(0, len(authorization_list)-1)]
+    corpo_email = f"""
+    <p>Seu código de verificação para o questionário é<p>
+    <h1><strong>{auth_code}</strong></h1>
+    <p>Esta é uma mensagem automática, não é necessário respondê-la.<p>
+    <br>
+    <hr>
+    <p>Esta é uma mensagem automática, não é necessário respondê-la.</p><br><br>
+    <a href="https://labeee.ufsc.br/pt-br/en-welcome"><img src="https://labeee.ufsc.br/sites/default/files/labeee_final_completo_maior.png" width="400" /></a>"""
+    msg = email.message.Message()
+    msg['Subject'] = f'CÓDIGO DE VERIFICAÇÃO - QAI em escritórios, LabEEE'
+    msg['From'] = 'escritorios.qai.bot@gmail.com'
+    msg['To'] = mail_person
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email)
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    return auth_code
 
-    submitted = st.form_submit_button("Pressione")
-    if submitted:
-        st.title('Seu código é "12AB5D"')
-        open('dados.txt', 'w').write(f'12AB5D/{cep}')
-
+def send_thanks_email(mail_person:str):
+    corpo_email = f"""
+    <h2>A equipe LabEEE agradece pela participação na pesquisa!</h2>
+    <br>
+    <hr>
+    <p>Esta é uma mensagem automática, não é necessário respondê-la.</p><br><br>
+    <a href="https://labeee.ufsc.br/pt-br/en-welcome"><img src="https://labeee.ufsc.br/sites/default/files/labeee_final_completo_maior.png" width="400" /></a>"""
+    msg = email.message.Message()
+    msg['Subject'] = f'CONFIRMAÇÃO DE PARTICIPAÇÃO - QAI em escritórios, LabEEE'
+    msg['From'] = 'escritorios.qai.bot@gmail.com'
+    msg['To'] = mail_person
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email)
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
