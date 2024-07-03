29da3a1dd52da796d82d2f7a829481dfeffd4c00
Zac-Milioli <zacmilioli@gmail.com>
2024-07-03 16:43:31 -0300

    FUNCIONA MEUDEUS

--- testpage_googlesheet.py
+++ testpage_googlesheet.py
@@ -1,9 +1,25 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+import gspread
+from google.oauth2.service_account import Credentials
 
-url = "https://docs.google.com/spreadsheets/d/not-real"
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+client = gspread.authorize(creds)
+sheet_id = "1T3D_x6qr4OjXQ6qMXJR0ZCywifJaHP1ck-PbZ2vT_2I"
+workbook = client.open_by_key(sheet_id)
+worksheet = workbook.worksheet('user')
 
-conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+conn = st.connection("gsheets", type=GSheetsConnection, ttl=10)
+data = conn.read(worksheet='user')
+st.dataframe(data, use_container_width=True)
 
-data = conn.read(spreadsheet=url, usecols=[0, 1])
-st.dataframe(data)
+name = st.text_input("name")
+id_ = st.text_input("id")
+text = st.text_input("text")
+
+col1, col2 = st.columns(2)
+if col1.button("Adicionar linha nova", use_container_width=True):
+    worksheet.append_row([name, id_, text])
+if col2.button("Recarregar a página", use_container_width=True):
+    st.rerun()
