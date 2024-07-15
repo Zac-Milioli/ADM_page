459475cd142b68cc55ae4bd4c8c4ce566b44527f
Zac-Milioli <zacmilioli@gmail.com>
2024-07-15 13:41:38 -0300

    feat: lista anti-injeção sql + teste para conexão com tableau

--- pages/ADM_searchpage.py
+++ pages/ADM_searchpage.py
@@ -8,7 +8,10 @@ st.title("Busca de local de trabalho")
 id_search = st.text_input(label="Insira o ID do seu local de trabalho", max_chars=8)
 _, col, _ = st.columns([3,1,3])
 if col.button(label='Buscar', use_container_width=True):
-    returned, status = get_build_info_by_id(id_=int(id_search))
+    try:
+        returned, status = get_build_info_by_id(id_=int(id_search))
+    except:
+        status = "ERROR"
     if status == "OK":
         return_list = returned.iloc[0].tolist()
         st.session_state['build_id'] = int(return_list.pop(0))
