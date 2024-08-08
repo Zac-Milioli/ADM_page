79b7da812a7da09550b2c36da671857604978064
Zac-Milioli <zacmilioli@gmail.com>
2024-08-08 12:54:59 -0300

    nome em string

--- ADM_firstpage.py
+++ ADM_firstpage.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(layout='wide',initial_sidebar_state='collapsed', page_title=f"ADM - {project}", page_icon = '📝')
+st.set_page_config(layout='wide',initial_sidebar_state='collapsed', page_title=f"ADM - VOSS", page_icon = '📝')
 from utils.config import *
 
 initialize_page()
